d0a0e067046875651bf544670970501577cfdcf1
Alix <alix@spacefoot.com>
2020-01-20 22:04:45 +0100

    rpc api added

---
+++ api/receiver.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/alixleger/cours1/entity"
+	"github.com/alixleger/cours1/mongodb"
+)
+
+const databaseName = "test"
+const serverURI = "mongodb://localhost:27017"
+
+// Persons is the method type of the RPC API
+type Persons struct {
+	DBClient *mongodb.MongoClient
+}
+
+// GetPersons is the RPC operator to get all mongodb person entities
+func (p *Persons) GetPersons(r *http.Request, args *string, result *[]entity.Person) error {
+	persons, err := p.DBClient.GetPersons()
+	if err == nil {
+		*result = persons
+	}
+
+	return err
+}
