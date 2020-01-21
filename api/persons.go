1b79ad5c5a8e9304c850ac928b7f3d1a23f78132
Alix <alix@spacefoot.com>
2020-01-21 16:34:46 +0100

    Persons RPC Api implemented

--- api/persons.go
+++ api/persons.go
@@ -21,3 +21,34 @@ func (p *Persons) GetPersons(r *http.Request, args *string, result *[]entity.Per
 
 	return err
 }
+
+// GetPerson is the RPC operator to get a mongodb person entity from an ID
+func (p *Persons) GetPerson(r *http.Request, args *string, result *entity.Person) error {
+	person, err := p.DBClient.GetPerson(*args)
+	if err == nil {
+		*result = person
+	}
+
+	return err
+}
+
+// InsertPerson is the RPC operator to insert a mongodb person entity
+func (p *Persons) InsertPerson(r *http.Request, args *entity.Person, result *string) error {
+	p.DBClient.InsertPerson(*args)
+	*result = "OK"
+	return nil
+}
+
+// UpdatePerson is the RPC operator to update a mongodb person entity
+func (p *Persons) UpdatePerson(r *http.Request, args *entity.Person, result *string) error {
+	p.DBClient.UpdatePerson(*args)
+	*result = "OK"
+	return nil
+}
+
+// DeletePerson is the RPC operator to delete a mongodb person entity
+func (p *Persons) DeletePerson(r *http.Request, args *string, result *string) error {
+	p.DBClient.DeletePerson(*args)
+	*result = "OK"
+	return nil
+}
